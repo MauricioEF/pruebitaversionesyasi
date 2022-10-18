f72ec562868b4dffa7021482dd63be7e0a30dac7
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2022-10-18 09:00:20 -0500

    Mis cambios felices

--- src/app.js
+++ src/app.js
@@ -8,3 +8,6 @@ app.listen(8080,()=>console.log("Listening"))
 app.get('/',(req,res)=>{
     res.send("Mis primeros cambios");
 })
+app.get('/miendpointJR',(req,res)=>{
+    res.send("Mi cambio feliz");
+})
